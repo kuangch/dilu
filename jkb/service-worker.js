511215cd4927324c4b80daf31bcbf6e9c647c6e1
kuangch <kuangch@dilusense.com>
2021-11-27 17:15:29 +0800

    添加健康宝

---
+++ jkb/service-worker.js
@@ -0,0 +1,37 @@
+importScripts("precache-manifest.a48c7d121f2a96d1573884af8f7a7a9b.js", "workbox-v4.3.1/workbox-sw.js");
+workbox.setConfig({modulePathPrefix: "workbox-v4.3.1"});
+/* ========================================
+ *  company : Dilusense
+ *   author : Kuangch
+ *     date : 2019/8/22
+ * ======================================== */
+
+// set the prefix and suffix of our sw's name
+workbox.core.setCacheNameDetails({
+    prefix: '3d-face',
+    suffix: 'v1.0.0',
+});
+// have our sw update and control a web page as soon as possible.
+workbox.core.skipWaiting();
+workbox.core.clientsClaim();
+
+// workbox.expiration.Plugin({
+//     purgeOnQuotaError: true
+// });
+
+// vue-cli3.0 supports pwa with the help of workbox-webpack-plugin, we need to get the precacheing list through this sentence.
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+// cache our data, and use networkFirst strategy.
+workbox.routing.registerRoute(
+    new RegExp('.*experiments\?.*'),
+    new workbox.strategies.NetworkFirst()
+);
+workbox.routing.registerRoute(
+    new RegExp('.*experiments/\\d'),
+    new workbox.strategies.NetworkFirst()
+)
+workbox.routing.registerRoute(
+    new RegExp('.*experiment_types.*'),
+    new workbox.strategies.NetworkFirst()
+)
