4863fadf1fc73b0be7d01496719a1bbcb2d9cc3e
kuangch <kuangch@dilusense.com>
2021-11-27 19:40:19 +0800

    缺失文件

---
+++ jkb2/precache-manifest.c308eef552ef3eb19c4303b3927e4dc0.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "161bf023dd936ae8d63f",
+    "url": "css/main.9dfa65a2.css"
+  },
+  {
+    "revision": "2fad952a20fbbcfd1bf2ebb210dccf7a",
+    "url": "fonts/element-icons.2fad952a.woff"
+  },
+  {
+    "revision": "6f0a76321d30f3c8120915e57f7bd77e",
+    "url": "fonts/element-icons.6f0a7632.ttf"
+  },
+  {
+    "revision": "cfa49246cf5428aab769956cd49e704d",
+    "url": "img/back.cfa49246.png"
+  },
+  {
+    "revision": "bcf5c3b86af0602fe80210e2d0f8e20f",
+    "url": "img/bj.bcf5c3b8.jpeg"
+  },
+  {
+    "revision": "543b6c959e1bb462facd6535c980c635",
+    "url": "img/close.543b6c95.png"
+  },
+  {
+    "revision": "d90db8ce2e49fd87dc4ccbc3fe5a2848",
+    "url": "img/duihao.d90db8ce.png"
+  },
+  {
+    "revision": "fb516045ddf3a7d1cb9ed1541a2c01df",
+    "url": "img/hulei.fb516045.gif"
+  },
+  {
+    "revision": "6971e16527ce45d94f1eddd80e049c69",
+    "url": "img/reg.6971e165.jpg"
+  },
+  {
+    "revision": "051bf49508e7109d808cfb09b7a60e69",
+    "url": "img/sm.051bf495.jpeg"
+  },
+  {
+    "revision": "96b98bab3ac7acfcfa65e74c2fbf79d8",
+    "url": "img/wenhao.96b98bab.png"
+  },
+  {
+    "revision": "5c921b9dd109b4b737d875500e94de85",
+    "url": "index.html"
+  },
+  {
+    "revision": "3a3130b5203931b96525",
+    "url": "js/XLSX.d55a60e1.js"
+  },
+  {
+    "revision": "5b2e9630941326c5293d",
+    "url": "js/chunk-vendors.13efca6a.js"
+  },
+  {
+    "revision": "161bf023dd936ae8d63f",
+    "url": "js/main.ce569743.js"
+  },
+  {
+    "revision": "ed244a067ed92f758195",
+    "url": "js/vendors~XLSX.c6404c9e.js"
+  },
+  {
+    "revision": "29bef70fcdf3dfeaf73ec91cc38e1a5e",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
